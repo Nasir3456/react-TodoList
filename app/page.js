a721a88a99a7b432c4ed153a018830d4a988557c
github_username <email_address>
2024-09-22 17:26:05 +0530

    react to-do list

--- app/page.js
+++ app/page.js
@@ -1,95 +1,56 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import React from 'react'
+import { useState } from "react";
 
-export default function Home() {
+
+const page = () => {
+  const [input, setinput] = useState("")
+  const [task, settask] = useState([])
+
+  const addTask = () =>{
+    let err = [...task];
+    err.push(input)
+    settask(err)
+    setinput("")
+  }
+
+  const handleDelete = (index) => {
+    let err = [...task];
+    err.splice(index, 1);
+    settask(err);
+  }
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <h1>TO-DO list with React.js</h1>
+      <div className='input-task'>
+        <input onChange={(e)=>{
+          setinput(e.target.value)
+        }} type='text' placeholder='Add a task' value={input} />
+        <button onClick={()=>{
+          addTask()
+        }} className='addBtn'>Add</button>
+      </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      <div className='container'>
+        <ul>
+          {
+            task.length > 0 && task.map((element,index)=>{
+              return <li key={index}>
+              <p>{element}</p>
+              <button onClick={()=>{
+                handleDelete(index);
+              }} className='deleteBtn'>Delete</button>
+            </li>  
+            })
+          }
+
+                  
+        </ul>
+      </div>
+    </>
+  )
 }
+
+
+
+export default page
